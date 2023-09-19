aba2b6c661a63f3817ec8061be192f00964ab8d8
yun <2696584863@qq.com>
2023-09-19 20:52:27 +0800

    second commit

--- src/main/java/atguigu/git/GitTest.java
+++ src/main/java/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world1");
+        System.out.println("hello world2");
     }
 }
