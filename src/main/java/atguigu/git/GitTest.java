92e416b8d721daa7350634b9f778d59042073f74
yun <2696584863@qq.com>
2023-09-19 21:03:33 +0800

    hot-fix commit2

--- src/main/java/atguigu/git/GitTest.java
+++ src/main/java/atguigu/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello world2");
         System.out.println("hello world3");
         System.out.println("hello world4");
+        System.out.println("hot-fix test!");
 
     }
 }
